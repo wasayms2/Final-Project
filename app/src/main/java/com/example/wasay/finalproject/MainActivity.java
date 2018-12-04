bdbabd1562c7a2fa3c9e80e67967d3e919738ce5
CullenStone <cps_stone@yahoo.com>
2018-12-03 21:02:33 -0600

    Worked on the buttons

--- app/src/main/java/com/example/wasay/finalproject/MainActivity.java
+++ app/src/main/java/com/example/wasay/finalproject/MainActivity.java
@@ -2,12 +2,30 @@ package com.example.wasay.finalproject;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
+    TextView txt;
+    Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        txt = findViewById(R.id.myTxt);
+        btn = findViewById(R.id.myButton);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                btn.setText("New Meme");
+                txt.setText("Name");
+            }
+        });
+
     }
+
 }
